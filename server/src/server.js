c0ca7754d4cc0e06ddfa5af187ace3f56163bca7
Dhruv Jain <dhruvjaincodes@gmail.com>
2023-11-23 13:02:25 +0530

    server update

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,9 @@
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
@@ -11,3 +17,4 @@ mongoose.connect(mongoDbUri);
 app.listen(port, () => {
   console.log(`API server started at ${port}`);
 });
+app.use(cors(corsOptions)) // Use this after the variable declaration
